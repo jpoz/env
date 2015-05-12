1786c9377d89ba166978d7609b9e1b7046b8a029
James Taco Pozdena <jpoz@simple.com>
2015-05-11 19:18:53 -0700

    Adding documentation for Error

--- decoder/error.go
+++ decoder/error.go
@@ -2,16 +2,19 @@ package decoder
 
 import "strings"
 
+// Error is a wrapper around a group of failed field errors
 type Error struct {
 	Errors map[string]error
 }
 
+// NewError returns a new Error with a valid map
 func NewError() Error {
 	return Error{
 		Errors: map[string]error{},
 	}
 }
 
+// Error returns a string of all the failed field names
 func (e Error) Error() string {
 	str := "Error decoding: "
 	fields := []string{}
